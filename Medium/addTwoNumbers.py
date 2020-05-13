0806d89b44c860a4c5dcd77ea1cb79b70d7faedd
Gage Anderson <gwanders380@gmail.com>
2020-05-13 11:22:00 -0400

    Solved the reccomended way, however my solution is confusing and inefficient, should work more on linked-list type problems

--- Medium/addTwoNumbers.py
+++ Medium/addTwoNumbers.py
@@ -4,7 +4,7 @@
 #        self.val = val
 #        self.next = next
 
-class Solution:
+class Solution1:
     def reverse(self, L: ListNode):
         prev = None
         next = None
@@ -55,3 +55,52 @@ class Solution:
     #Read the problem statement carefully, you were returning the wrong thing, needed to return a linked list
     
     #Handling the default objects that LeetCode gives you is a little confusing...
+
+    # Definition for singly-linked list.
+#class ListNode:
+#     def __init__(self, val=0, next=None):
+#        self.val = val
+#        self.next = next
+
+class Solution2:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        L = ListNode(0,None)
+        L1Ended = False
+        L2Ended = False
+        curr1 = l1
+        curr2 = l2
+        curr3 = L
+        sum = 0
+        while((not L1Ended) or (not L2Ended)):                        
+            if(L1Ended): 
+                val1 = 0
+            else:
+                val1 = curr1.val
+            if(L2Ended): 
+                val2 = 0
+            else:
+                val2 = curr2.val
+            
+            if(not L1Ended):
+                curr1 = curr1.next
+                if(curr1 is None): 
+                    L1Ended = True
+            if(not L2Ended):
+                curr2 = curr2.next
+                if(curr2 is None):
+                    L2Ended = True
+            
+            sum = val1 + val2 + sum
+            rem = sum % 10
+            quot = sum // 10
+            sum = quot
+            print(f"sum = {sum}")
+            curr3.val = rem
+            print(f"curr3.val = {curr3.val}")
+            curr3.next = ListNode(quot,None)
+            if(L1Ended and L2Ended and sum == 0):
+                curr3.next = None
+                break
+            curr3 = curr3.next
+        
+        return L
